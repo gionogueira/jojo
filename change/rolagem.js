91dcd433b7a12492f2c185eebaddac87931f10de
Giovanna Rodrigues Nogueira <giovannarnogueira@gmail.com>
2017-07-02 23:15:00 -0300

    Resolver problemas de navegação para o usuário
    Após análise heurística foram descobertos os problemas que
    possivelmente dificultariam a experiência do usuário.
    
    * Adicionado FontAwesome para substituir ícones.
    * Consertado redirecionamento da logo.
    * Mudança do nome contato para fale conosco.
    * Adicionado links ao nome dos criadores do site.
    * Mudança dos ícones do footer.
    * Reorganização e adição das informações do footer.
    * Implementação para funcionmento do mapa na página como chegar.
    * Alinhamento e adição de ícones nas infformações ao lado do mapa.
    * Incremento do JavaScript para funcionamento do bot.
    * Implementação do redirecinamento para página confirm.
    * Adição do hover ao menu.
    * Mudança no alinhamento e cor dos botões do formulário.
    * Adicionada página confirm.
    * Adicionado check para requerimento dos campos do formulário.
    * Adicionada máscara para número de telefone.
    * Excluídas legendas das imagens do carossel.
    
    Resolves #1

--- change/rolagem.js
+++ change/rolagem.js
@@ -75,3 +75,73 @@ window.onscroll = function(e) {
  */
 offSetManager();
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('a[href="#cabe"]').fadeIn();
+        } else {
+            $('a[href="#cabe"]').fadeOut();
+        }
+    });
+
+    $('a[href="#cabe"]').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+    $('#check').on('submit', function(e){
+      e.preventDefault();
+      var data={
+        nome: $('input[name="nome"]').val(),
+        email: $('input[name="email"]').val(),
+        telefone: $('input[name="telefone"]').val(),
+        mensagem: $('textarea[name="mensagem"]').val()
+      }
+      axios.post('http://bot-jojo-patsserie.herokuapp.com/mensagem', data)
+        .then(function(response) {
+          window.location.replace('/confirm.html');
+        }).catch(function(e) {
+          window.location.replace('/confirm.html');
+        })
+        /*
+      $.ajax({
+        type: 'POST',
+        url:'http://bot-jojo-patsserie.herokuapp.com/mensagem',
+        data:JSON.stringify(data),
+        dataType:'json',
+        contentType: 'application/json; charset=utf-8',
+        success: function(){
+          window.location.replace('confirm.html');
+        }
+      });
+      */
+    });
+
+});
+
+function initMap() {
+        var uluru = {lat: -5.8751765, lng: -35.1805885};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 17,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+}
+
+function mascara(o,f){
+    v_obj=o
+    v_fun=f
+    setTimeout("execmascara()",1)
+}
+function execmascara(){
+    v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+    v=v.replace(/\D/g,"");
+    v=v.replace(/^(\d{2})(\d)/g,"($1) $2");
+    v=v.replace(/(\d)(\d{4})$/,"$1-$2");
+    return v;
+}
